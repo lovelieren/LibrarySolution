16369f54b6afc9668968779d4ea14dfc8112acff
ZhangQiang <love_lieren@126.com>
2017-04-08 15:06:34 +0800

    V0.1
    Initial

--- ThreadExample/Program.cs
+++ ThreadExample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -10,7 +11,13 @@ namespace ThreadExample
     {
         static void Main(string[] args)
         {
-            
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+        }
+
+        static void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
